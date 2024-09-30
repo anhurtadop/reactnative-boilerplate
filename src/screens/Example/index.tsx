f54224af7f5856087a35cb10a533e5336bcd7737
Ricardo Rico <ricardo.rico@gmail.com>
2024-09-29 21:02:59 -0500

    feat: get token for firebase cloud messaging on Example screen

--- src/screens/Example/index.tsx
+++ src/screens/Example/index.tsx
@@ -1,14 +1,28 @@
 import { StackParamList } from '@/router/Stacks/MainStack';
 import { exampleAction, setAuth } from '@/store/example/action';
 import { selectExampleName } from '@/store/selectors';
+import messaging from '@react-native-firebase/messaging';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button, Text, View } from 'react-native';
+import { Button, PermissionsAndroid, Text, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import styles from './styles';
 
+messaging().onTokenRefresh((token) => console.log('[onTokenRefresh] NEW NOTIFICATION TOKEN', token));
+async function getTokenForPushNotifications() {
+  try {
+    const msgService = messaging();
+    await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+    await msgService.registerDeviceForRemoteMessages();
+    await msgService.deleteToken();
+    const token = await msgService.getToken();
+    console.log('[getToken] GOT TOKEN', token);
+  } catch (e) {
+    console.log('ERROR IN GETTING TOKEN', e);
+  }
+}
 export interface ExampleProps extends NativeStackScreenProps<StackParamList> {}
 const Example: React.FC<ExampleProps> = () => {
   const { t } = useTranslation();
@@ -32,6 +46,7 @@ const Example: React.FC<ExampleProps> = () => {
         }}
         title={t('sign-in-button')}
       />
+      <Button onPress={getTokenForPushNotifications} title={t('get-token-button')} />
     </View>
   );
 };
